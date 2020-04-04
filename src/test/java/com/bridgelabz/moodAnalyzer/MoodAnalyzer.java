206e8cbea6066b4af3960d965e28d30b70b6536f
roshan1718 <roshanshinde1717@gmail.com>
2020-04-04 22:02:23 +0530

    ADDED:UC1 code to Check mood respond happy or sad

---
+++ src/test/java/com/bridgelabz/moodAnalyzer/MoodAnalyzer.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.moodAnalyzer;
+
+public class MoodAnalyzer {
+
+    public static void main(String[] args) {
+        System.out.println("***************Welcome To Mood Analyzer***************");
+    }
+    // Method that return the mood
+    public String moodAnalyzer(String mood) {
+        if (mood.equals("I am in sad mood")) {
+            return ("sad");
+        } else if (mood.equals("I am in any mood")) {
+            return ("happy");
+        }
+        return (mood);
+    }
+}
