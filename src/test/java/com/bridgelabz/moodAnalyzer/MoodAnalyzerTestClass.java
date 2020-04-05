c2cf6f54cced073cb356f32d36ee69c5c114f161
roshan1718 <roshanshinde1717@gmail.com>
2020-04-05 08:19:27 +0530

    ADDED:TC5.3 code to Constructor when improper should throw metho not found exception

--- src/test/java/com/bridgelabz/moodAnalyzer/MoodAnalyzerTestClass.java
+++ src/test/java/com/bridgelabz/moodAnalyzer/MoodAnalyzerTestClass.java
@@ -64,7 +64,7 @@ public class MoodAnalyzerTestClass {
         }
     }
     @Test
-    public void givenClassName_WhenInimproper_ThenThrowMoodAnalysisException() {
+    public void givenConstructorName_WhenImproper_ThenThrowMoodAnalysisException() {
         try{
             MoodAnalyzer mood = new MoodAnalyzer();
             MoodAnalyserFactory.getConstructor("MoodAnalyzer",Integer.class);
@@ -94,6 +94,16 @@ public class MoodAnalyzerTestClass {
             Assert.assertEquals(true,result);
         }catch(MoodAnalysisException e){
             Assert.assertEquals(MoodAnalysisException.MyException_Type.CLASS_NOT_FOUND,e.type);
+        }
+    }
+    @Test
+    public void givenConstructorWithParameter_WhenImproper_ThenThrowMethodNotFoundException() {
+        try{
+            MoodAnalyzer mood = new MoodAnalyzer("i am in happy mood");
+            Object returnMood=MoodAnalyserFactory.getParameterizConstructor("MoodAnalyzer",Integer.class,"i am in happy mood");
+            boolean result = returnMood.equals(mood);
+        }catch(MoodAnalysisException e){
+            Assert.assertEquals(MoodAnalysisException.MyException_Type.METHOD_NOT_FOUND,e.type);
         }
     }
 }
